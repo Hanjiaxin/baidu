e64122e118204b841ceda28f1bf8d3d68c12dcce
Hanjiaxin <1123070132@qq.com>
2020-03-18 15:55:48 +0800

    baiduDemo

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        htmlmin: {
+            options: {
+                collapseWhitespace: true,
+                preserveLineBreaks: false
+            },
+            files: {
+                src: 'dist/index.html',
+                dest: 'dist/index.html'
+            }
+        },
+        cssmin: {
+            'dist/demo.css': 'demo.css'
+        },
+        uglify: {
+            release: {
+                files: {
+                    'dist/demo.min.js': 'dist/demo.js',
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('release', ['uglify', 'cssmin', 'htmlmin']);
+};
